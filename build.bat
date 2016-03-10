ebac36cf8f7dd0b06fdebb0f788a5dc4dbf318aa
vladnega <negacevschi.vladimir@gmail.com>
2016-03-10 15:43:24 +0000

    generate HTTP examples from FlexSearch tests

--- build.bat
+++ build.bat
@@ -3,10 +3,14 @@
 echo Downloading FlexSearch Core
 git submodule update --init --recursive
 
-echo Building the FlexSearch API
 cd flexsearch
 call build.bat api
+call build.bat target=HttpTests
 cd ..
 
 echo Copying the generated api.html file
 copy flexsearch\documentation\api.html includes\api.html
+
+echo Copying the generated examples
+if not exist "docs\examples" mkdir "docs\examples"
+copy flexsearch\documentation\docs\data\*.* docs\examples\
